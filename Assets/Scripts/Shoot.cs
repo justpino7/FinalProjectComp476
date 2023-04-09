3e932dfbb0e666ea6d97f2ca4fd34fe0541809e8
justpino7 <nicopino_7@hotmail.com>
2023-04-09 04:18:12 -0400

    Implemented Basic Shooting

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -1,18 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 public class Shoot : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Rigidbody bullet;
+    public Transform crosshair;
+    public float velocity = 10.0f;
+    float _destroyTime = 3.0f;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1") || Input.GetKeyDown("space"))
+        {
+            Rigidbody newBullet = Instantiate(bullet, transform.position, transform.rotation) as Rigidbody;
+
+            // Get the Arwing's velocity
+            Rigidbody arwingRigidbody = GetComponentInParent<Rigidbody>();
+            Vector3 arwingVelocity = Vector3.zero;
+            if (arwingRigidbody != null)
+            {
+                arwingVelocity = arwingRigidbody.velocity;
+            }
+
+            // Calculate the direction to shoot the bullet towards the crosshair
+            Vector3 shootDirection = (crosshair.position - transform.position).normalized;
+
+            // Add the Arwing's velocity to the bullet's velocity and aim towards the crosshair
+            newBullet.AddForce((shootDirection * velocity) + arwingVelocity, ForceMode.VelocityChange);
+            Destroy(newBullet.gameObject, _destroyTime);
+        }
     }
 }
