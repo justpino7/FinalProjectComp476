011b6e72f3f3458183d51a861f65ce982d7f6df4
justpino7 <nicopino_7@hotmail.com>
2023-04-10 00:58:00 -0400

    Implemented barrel roll, boost and brake

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,6 +11,9 @@ public class PlayerMovement : MonoBehaviour
     public float lookSpeed = 340;
     public float forwardSpeed = 12;
 
+    public float barrelRollSpeed = 720.0f; // 720 degrees per second is a full rotation in 0.5 seconds
+    public float barrelRollCooldown = 2.0f; // 2 seconds cooldown
+
     private Transform playerModel;
 
 
@@ -18,6 +21,24 @@ public class PlayerMovement : MonoBehaviour
     public CinemachineDollyCart dolly;
 
     public bool joystick = true;
+
+    private bool inBarrelRoll = false;
+    private float lastBarrelRollTime = -Mathf.Infinity;
+
+    public float boostMultiplier = 2.0f; // Multiply forward speed by this value when boosting
+    public float breakMultiplier = 0.5f; // Multiply forward speed by this value when breaking
+    public float boostBreakDuration = 3.0f; // Maximum duration for boost or break
+    public float boostBreakCooldown = 2.0f; // Cooldown duration after boost or break ends
+
+    private float lastBoostBreakTime = -Mathf.Infinity;
+    private bool inBoostOrBreak = false;
+
+    // Speed boost brake
+    public CinemachineVirtualCamera virtualCamera;
+    public float boostFOV = 70.0f; // Increased FOV for the boost
+    public float breakFOV = 50.0f; // Decreased FOV for the break
+    public float normalFOV = 60.0f; // Normal FOV when not boosting or breaking
+    public float fovLerpSpeed = 5.0f; // Speed for lerping between FOVs
 
 
     private void Start()
@@ -33,9 +54,90 @@ public class PlayerMovement : MonoBehaviour
 
         LocalMove(h, v, xySpeed);
         RotationLook(h, v, lookSpeed);
-        HorizontalLean(playerModel, h, 80, 0.1f);
 
+        if (!inBarrelRoll) // Only apply lean when not in barrel roll
+        {
+            HorizontalLean(playerModel, h, 80, 0.1f);
+        }
         ClampPosition();
+
+        // Barrel Roll Input
+        if (Input.GetKeyDown(KeyCode.Q) && !inBarrelRoll && Time.time - lastBarrelRollTime > barrelRollCooldown)
+        {
+            StartCoroutine(BarrelRoll());
+        }
+
+        // Boost or Break Input
+        if ((Input.GetKey(KeyCode.X) || Input.GetKey(KeyCode.Z)) && !inBoostOrBreak && Time.time - lastBoostBreakTime > boostBreakCooldown)
+        {
+            StartCoroutine(HandleBoostBreak());
+        }
+    }
+
+    IEnumerator BarrelRoll()
+    {
+        inBarrelRoll = true;
+        lastBarrelRollTime = Time.time;
+
+        float barrelRollDuration = 360f / barrelRollSpeed;
+        float elapsedTime = 0;
+
+        while (elapsedTime < barrelRollDuration)
+        {
+            float rotationAmount = barrelRollSpeed * Time.deltaTime;
+            playerModel.Rotate(0, 0, rotationAmount);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        Vector3 finalEulerAngles = playerModel.localEulerAngles;
+        finalEulerAngles.z = Mathf.Round(finalEulerAngles.z / 360) * 360;
+        playerModel.localEulerAngles = finalEulerAngles;
+
+        inBarrelRoll = false;
+    }
+
+    IEnumerator HandleBoostBreak()
+    {
+        inBoostOrBreak = true;
+        float elapsedTime = 0;
+
+        // Store the original forward speed
+        float originalForwardSpeed = dolly.m_Speed;
+
+        while (elapsedTime < boostBreakDuration && (Input.GetKey(KeyCode.X) || Input.GetKey(KeyCode.Z)))
+        {
+            elapsedTime += Time.deltaTime;
+
+            // Update the dolly speed and FOV based on the input
+            if (Input.GetKey(KeyCode.X))
+            {
+                dolly.m_Speed = originalForwardSpeed * boostMultiplier;
+                virtualCamera.m_Lens.FieldOfView = Mathf.Lerp(virtualCamera.m_Lens.FieldOfView, boostFOV, fovLerpSpeed * Time.deltaTime);
+            }
+            else if (Input.GetKey(KeyCode.Z))
+            {
+                dolly.m_Speed = originalForwardSpeed * breakMultiplier;
+                virtualCamera.m_Lens.FieldOfView = Mathf.Lerp(virtualCamera.m_Lens.FieldOfView, breakFOV, fovLerpSpeed * Time.deltaTime);
+            }
+
+            yield return null;
+        }
+
+        // Reset dolly speed to the original speed
+        dolly.m_Speed = originalForwardSpeed;
+
+        // Lerp back to the normal FOV
+        while (Mathf.Abs(virtualCamera.m_Lens.FieldOfView - normalFOV) > 0.1f)
+        {
+            virtualCamera.m_Lens.FieldOfView = Mathf.Lerp(virtualCamera.m_Lens.FieldOfView, normalFOV, fovLerpSpeed * Time.deltaTime);
+            yield return null;
+        }
+
+        virtualCamera.m_Lens.FieldOfView = normalFOV;
+
+        lastBoostBreakTime = Time.time;
+        inBoostOrBreak = false;
     }
 
     // Allows the spaceship to follow the cursor
